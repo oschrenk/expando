3d490264ee726504fa5a2bd1957932c3a6fc8722
Oliver Schrenk <oliver.schrenk@gmail.com>
2018-04-24 16:14:33 +0200

    Set incoming cookies for redirects
    http://ift.tt/2yTbXx1 https://pubs.acs.org/doi/10.1021/acs.iecr.7b03729

--- src/main/scala/com/oschrenk/expando/Expando.scala
+++ src/main/scala/com/oschrenk/expando/Expando.scala
@@ -2,14 +2,13 @@ package com.oschrenk.expando
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.headers.Location
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{Cookie, Location, `Set-Cookie`}
 import akka.stream.{ActorMaterializer, Materializer}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 import scala.io.Source
-import scala.util.{Failure, Success}
 
 object Expando {
   def main(args: Array[String]): Unit = {
@@ -17,44 +16,46 @@ object Expando {
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    def redirectOrResult(originalUrl: Uri, newLocation: Option[Uri], response: HttpResponse)(implicit materializer: Materializer): Future[Uri] =
-    response.status match {
-      case StatusCodes.Found | StatusCodes.MovedPermanently | StatusCodes.SeeOther ⇒
-        val location = response.header[Location]
-        if (location.isEmpty) {
-          println(s"${response.status} $originalUrl")
-        }
+    // akka http does not follow redirects automatically
+    // see https://github.com/akka/akka-http/issues/195
+    def redirectOrResult(originalUrl: Uri, newLocation: Option[Uri], response: HttpResponse)(implicit materializer: Materializer): Future[Uri] = {
+      response.status match {
+        case StatusCodes.Found | StatusCodes.MovedPermanently | StatusCodes.SeeOther | StatusCodes.TemporaryRedirect ⇒
+          val location = response.header[Location]
+          val cookies = response.header[`Set-Cookie`].map(_.cookie).map(c => Cookie(c.name, c.value))
 
-        val newUri = location.get.uri
-        response.discardEntityBytes()
+          if (location.isEmpty) {
+            println(s"${response.status} $originalUrl")
+          }
 
-        // change to GET method as allowed by https://tools.ietf.org/html/rfc7231#section-6.4.3
-        Http().singleRequest(HttpRequest(method = HttpMethods.GET, uri = newUri))
-          .flatMap(res => redirectOrResult(originalUrl, location.map(_.uri), res))
-      // TODO: what to do on an error? Also report the original request/response?
-      // TODO: also handle 307, which would require resending POST requests
-      case _ ⇒
-        response.discardEntityBytes()
-        Future.successful(newLocation.getOrElse(originalUrl))
+          val newUri = location.get.uri
+          response.discardEntityBytes()
+
+          // change to GET method as allowed by https://tools.ietf.org/html/rfc7231#section-6.4.3
+          Http().singleRequest(HttpRequest(method = HttpMethods.GET, uri = newUri, headers = cookies.toList))
+            .flatMap(res => redirectOrResult(originalUrl, location.map(_.uri), res))
+        case _ ⇒
+          response.discardEntityBytes()
+          Future.successful(newLocation.getOrElse(originalUrl))
+      }
     }
 
-    val urls = Source.fromFile("/Users/oliver/Downloads/urls.txt", "UTF-8").getLines().toSeq
-    val result: Future[Seq[HttpResponse]] = Future.sequence{urls.map{ url =>
-      // akka http does not follow redirects automatically
-      // see https://github.com/akka/akka-http/issues/195
-    val f = Http().singleRequest(HttpRequest(uri = url))
-      f.onComplete {
-        case Success(res) => redirectOrResult(url, None, res).map{ newLoc =>
-          println(s"$url $newLoc")
+    val urls = Source.fromFile("/Users/oliver/Downloads/urls.txt", "UTF-8").getLines()
+    val result = Future.sequence{
+      urls.map { url =>
+        Http().singleRequest(HttpRequest(uri = url))
+          .flatMap(res => redirectOrResult(url, None, res).map{ newLoc =>
+            println(s"$url $newLoc")
+          }).recover{
+          case err =>
+            println(s"$url $err")
         }
-        case Failure(error) =>
-          println(s"$url $error")
       }
-      f
-    }}
+    }
 
-    Await.result(result, Duration.Inf)
-    result.map(_ => system.terminate())
+    Await.result(result, Duration.Inf).foreach{
+      _ => system.terminate()
+    }
   }
 
 }
