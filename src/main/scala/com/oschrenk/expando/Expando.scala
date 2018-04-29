1018b9bc2d1b7adfc43b45204dbafe026bb5499b
Federico Ragona <federico.ragona@gmail.com>
2018-04-29 20:26:01 +0200

    Follow redirects by feeding URIs to another source
    Pros:
    - follows redirects without recursion (inspiration: http://koff.io/posts/pagination-and-streams/)
    
    Cons:
    - sequential: didn't find a way to parallelize the flow yet

--- src/main/scala/com/oschrenk/expando/Expando.scala
+++ src/main/scala/com/oschrenk/expando/Expando.scala
@@ -12,112 +12,88 @@ import scala.concurrent.{ExecutionContextExecutor, Future, TimeoutException}
 import scala.io.{Source => FileSource}
 import scala.util.{Failure, Success}
 
-
-sealed trait ExpandedUri
-case class NoRedirect(source: Uri) extends ExpandedUri
-case class WithRedirect(source: Uri, target: Uri) extends ExpandedUri
-case class Failed(source: Uri, error: String) extends ExpandedUri
-
 object Expando {
+  case class Resource(uri: Uri, cookies: Option[Cookie]) {
+    override def toString() = s"$uri, cookies: $cookies"
+  }
 
   def main(args: Array[String]): Unit = {
     implicit val system: ActorSystem = ActorSystem()
     implicit val materializer: ActorMaterializer = ActorMaterializer()
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    def request(url: Uri, cookie: Option[Cookie] = None): Future[Either[String, HttpResponse]] = {
-      timeout(Http()
-        .singleRequest(HttpRequest(
-          method = HttpMethods.GET,
-          uri = url,
-          headers = cookie.toList)))
-        .map(Right.apply)
-        .recover{ case e => Left(e.getMessage)}
-    }
-
-    def redirectOrResult(originalUrl: Uri, newLocation: Option[Uri], response: HttpResponse)(implicit materializer: Materializer): Future[Either[String, Uri]] = {
+    def redirectOrResult
+        (original: Resource, response: HttpResponse)
+        (implicit materializer: Materializer): Option[(Resource, Unit)] = {
       response.status match {
         case StatusCodes.Found | StatusCodes.MovedPermanently | StatusCodes.SeeOther | StatusCodes.TemporaryRedirect ⇒
-          response.discardEntityBytes()
+          println(s"Got redirected with ${response.status} on $original")
 
           // deal with cookie protection schemes
           val cookies = response.header[`Set-Cookie`].map(_.cookie).map(c => Cookie(c.name, c.value))
 
           // deal with malformed location values containing whitespaces
-          val location = response.headers.find(h => h.lowercaseName().equals("location"))
+          response
+            .headers
+            .find(_.lowercaseName().equals("location"))
             .map { header =>
               try {
                 Uri.apply(header.value().replace(" ", "%20"))
               }
               catch {
-                case e: IllegalUriException => throw new IllegalArgumentException(s"Malformed Location header |${header.value()}| for $originalUrl: ${e.getMessage}")
+                case e: IllegalUriException =>
+                  throw new IllegalArgumentException(s"Malformed Location header |${header.value()}| for $original: ${e.getMessage}")
               }
-             }
+            }
+            .map {
+              case uri if uri.isRelative =>
+                (Resource(uri.resolvedAgainst(original.uri), cookies), ())
+              case uri  =>
+                (Resource(uri, cookies), ())
+            }
+        case other ⇒
+          println(s"Got $other on $original")
 
-          // deal with relative urls in location value
-          val newUri = location match {
-            case Some(uri) if uri.isRelative =>
-              uri.resolvedAgainst(newLocation.getOrElse(originalUrl))
-            case Some(uri)  =>
-              uri
-            case None =>
-              throw new IllegalArgumentException(s"empty location header on redirect for $originalUrl")
-          }
-
-          request(newUri, cookies).flatMap {
-            case Left(error) => Future.successful(Left(error))
-            case Right(res) => redirectOrResult(originalUrl, Some(newUri), res)
-          }
-        case _ ⇒
-          response.discardEntityBytes()
-          Future.successful(Right(newLocation.getOrElse(originalUrl)))
+          // marks the end of the expansion for this URI,
+          // so that the Source can proceed with the next
+          None
       }
     }
 
-    def timeout[T](f: Future[T]): Future[T] = {
-      import akka.pattern.after
-      val a = after(duration = Config.Timeout, using = system.scheduler)(Future.failed(new TimeoutException("Future timed out!")))
-      Future.firstCompletedOf(Seq(f, a))
-    }
-
-    def followRedirectOrResult(uri: Uri): Future[ExpandedUri] = {
-      if (uri.path.isEmpty) {
-        Future.successful(NoRedirect(uri))
-      } else {
-        request(uri)
-          .flatMap {
-            case Left(error) => Future.successful(Failed(uri, error))
-            case Right(res) =>
-              redirectOrResult(uri, None, res)
-                .recover{ case e => Left(e.getMessage)}
-                .map {
-                  case Left(error) => Failed(uri, error)
-                  case Right(newUri) => if (uri == newUri) NoRedirect(uri) else WithRedirect(uri, newUri)
-                }
-          }
-      }
-    }
-
-    val toUri: Flow[String, Uri, _] = Flow[String]
-      .map(Uri.apply)
-    val expandUri: Flow[Uri, ExpandedUri, _] = Flow[Uri]
-      .mapAsyncUnordered(Config.Parallelism)(followRedirectOrResult)
-    val printExpandedUri: Sink[ExpandedUri, Future[Done]] = Sink.foreach {
-      case NoRedirect(uri) =>
-        println(s"$uri 2xx $uri")
-      case WithRedirect(from, to) =>
-        println(s"$from 3xx $to")
-      case Failed(uri, error) =>
-        println(s"$uri 5xx $error")
-    }
     val urlSource =
       Source
-        .fromIterator(() => FileSource.fromFile(Config.Source.Path.get, Config.Source.Encoding).getLines())
+        .fromIterator(() => FileSource.fromFile(
+          Config.Source.Path.get,
+          Config.Source.Encoding).getLines())
+
+    def resources(originalUri: Resource): Source[Unit, _] = {
+      def resolve(what: Resource): Future[Option[(Resource, Unit)]] =
+        Http()
+          .singleRequest(HttpRequest(
+            method = HttpMethods.GET,
+            uri = what.uri,
+            headers = what.cookies.toList))
+          .map { res =>
+            res.discardEntityBytes(materializer)
+            redirectOrResult(what, res)
+          }
+          .recover {
+            case ex =>
+              println(s"Got FAILURE on $what. Exception: ${ex.getMessage}")
+              None
+          }
+
+      Source
+        .unfoldAsync[Resource, Unit](originalUri) { uri =>
+          println(s"Expanding $uri")
+          resolve(uri)
+        }
+    }
 
     urlSource
-      .via(toUri)
-      .via(expandUri)
-      .runWith(printExpandedUri)
+      .via(Flow[String].map(uri => Resource(Uri(uri), None)))
+      .flatMapConcat(resources)
+      .runWith(Sink.ignore)
       .onComplete {
         case Success(_) =>
           println("http://i.am.done.com 200 http://i.am.done.com")
